a600cf8767a241127fd69ff1db77c2d473de8b11
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-09-26 20:54:52 +0200

    Restructurings

--- src/test/java/com/alisarrian/KataTest.java
+++ src/test/java/com/alisarrian/KataTest.java
@@ -1,5 +1,6 @@
 package com.alisarrian;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,49 +8,49 @@ import static org.junit.jupiter.api.Assertions.*;
 class KataTest {
     @Test
     void shouldReturnMinus1WhenNull() {
-        assertEquals(-1, nthPowerOf(null, 1));
+        Assertions.assertEquals(-1, nthPowerOf(null, 1));
     }
 
     @Test
     void shouldReturnMinus1WhenEmptyArray() {
         int[] array = {};
-        assertEquals(-1, nthPowerOf(array, 0));
+        Assertions.assertEquals(-1, nthPowerOf(array, 0));
     }
 
     @Test
     void shouldReturnMinus1WhenNegativeIndex() {
         int[] array = {1};
-        assertEquals(-1, nthPowerOf(array, -1));
+        Assertions.assertEquals(-1, nthPowerOf(array, -1));
     }
 
     @Test
     void shouldReturn1WhenIndexIsEqual0() {
         int[] array = {1};
-        assertEquals(1, nthPowerOf(array, 0));
+        Assertions.assertEquals(1, nthPowerOf(array, 0));
     }
 
     @Test
     void shouldReturn25WhenIndexIsEqual0() {
         int[] array = {5};
-        assertEquals(5 * 5, nthPowerOf(array, 0));
+        Assertions.assertEquals(5 * 5, nthPowerOf(array, 0));
     }
 
     @Test
     void shouldReturnThePowerOfTheValueAtTheSpecifiedIndex() {
         int[] array = {1, 3, 5};
-        assertEquals(5 * 5, nthPowerOf(array, 2));
+        Assertions.assertEquals(5 * 5, nthPowerOf(array, 2));
     }
 
     @Test
     void shouldReturnMinus1WhenIndexIsEqualToArraySize() {
         int[] array = {1, 3, 5};
-        assertEquals(-1, nthPowerOf(array, 3));
+        Assertions.assertEquals(-1, nthPowerOf(array, 3));
     }
 
     @Test
     void shouldReturnMinus1WhenIndexIsGreaterThanArraySize() {
         int[] array = {1, 3, 5};
-        assertEquals(-1, nthPowerOf(array, 5));
+        Assertions.assertEquals(-1, nthPowerOf(array, 5));
     }
 
     private int nthPowerOf(int[] array, int index) {
