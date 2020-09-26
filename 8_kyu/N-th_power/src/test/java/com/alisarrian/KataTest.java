9cb10bf92ceee147a36238b5daad23f56cedf4cf
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-09-26 13:05:44 +0200

    Provide null as one of the parameters

--- 8_kyu/N-th_power/src/test/java/com/alisarrian/KataTest.java
+++ 8_kyu/N-th_power/src/test/java/com/alisarrian/KataTest.java
@@ -1,7 +1,14 @@
 package com.alisarrian;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class KataTest {
+    @Test
+    void shouldReturnMinus1_whenNull() {
+        assertEquals(-1, Kata.nthPower(null, 1));
+    }
+
 
 }
