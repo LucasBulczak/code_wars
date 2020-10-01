ef02a5d18dda469becc830607c708e0a3d6391b6
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-10-01 07:05:36 +0200

    Adds implementation provided at codewars

--- src/main/java/com/alisarrian/_8kyu/GrassHopper.java
+++ src/main/java/com/alisarrian/_8kyu/GrassHopper.java
@@ -1,4 +1,7 @@
 package com.alisarrian._8kyu;
 
 public class GrassHopper {
+    public static int summation(int n) {
+        return 0;
+    }
 }
--- src/test/java/com/alisarrian/_8kyu/GrassHopperTest.java
+++ src/test/java/com/alisarrian/_8kyu/GrassHopperTest.java
@@ -1,7 +1,18 @@
 package com.alisarrian._8kyu;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GrassHopperTest {
-
+    @Test
+    public void test1() {
+        assertEquals(1,
+                GrassHopper.summation(1));
+    }
+    @Test
+    public void test2() {
+        assertEquals(36,
+                GrassHopper.summation(8));
+    }
 }
