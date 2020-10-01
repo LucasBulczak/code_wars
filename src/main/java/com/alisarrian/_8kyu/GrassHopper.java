95a3800df922c738e8b701f67941821be319ee30
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-10-01 06:53:24 +0200

    Add empty classes for GrassHopper

---
+++ src/main/java/com/alisarrian/_8kyu/GrassHopper.java
@@ -0,0 +1,4 @@
+package com.alisarrian._8kyu;
+
+public class GrassHopper {
+}
---
+++ src/test/java/com/alisarrian/_8kyu/GrassHopperTest.java
@@ -0,0 +1,7 @@
+package com.alisarrian._8kyu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GrassHopperTest {
+
+}
