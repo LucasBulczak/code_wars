562a6a3edbc99ac09b93ae108ab82cc12b88e2b0
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-10-02 07:42:18 +0200

    GREEN: Silly implementation

--- src/main/java/com/alisarrian/_8kyu/GrassHopper.java
+++ src/main/java/com/alisarrian/_8kyu/GrassHopper.java
@@ -2,6 +2,8 @@ package com.alisarrian._8kyu;
 
 public class GrassHopper {
     public static int summation(int n) {
-        return 1;
+        return n == 1
+                ? 1
+                : 3;
     }
 }
