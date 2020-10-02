d82c3e7906770d295abbd0c02d03f9dad662c45c
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-10-02 07:48:10 +0200

    REFACTOR: Changes conditional statements to loop

--- src/main/java/com/alisarrian/_8kyu/GrassHopper.java
+++ src/main/java/com/alisarrian/_8kyu/GrassHopper.java
@@ -2,12 +2,10 @@ package com.alisarrian._8kyu;
 
 public class GrassHopper {
     public static int summation(int n) {
-        if (n == 1) {
-            return 1;
-        } else if (n == 2) {
-            return 3;
-        } else {
-            return 6;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
         }
+        return sum;
     }
 }
