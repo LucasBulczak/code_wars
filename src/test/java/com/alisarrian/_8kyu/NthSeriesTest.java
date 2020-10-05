c3f96613abdb0342aaff8d48889b1b684f4e5ab2
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-10-05 16:46:52 +0200

    GREEN: uncomment all tests provided by code wars

--- src/test/java/com/alisarrian/_8kyu/NthSeriesTest.java
+++ src/test/java/com/alisarrian/_8kyu/NthSeriesTest.java
@@ -20,16 +20,16 @@ class NthSeriesTest {
         assertEquals("1.39", NthSeries.seriesSum(3));
     }
 
-//    @Test
-//    public void test1() {
-//        assertEquals("1.57", NthSeries.seriesSum(5));
-//    }
-//    @Test
-//    public void test2() {
-//        assertEquals("1.77", NthSeries.seriesSum(9));
-//    }
-//    @Test
-//    public void test3() {
-//        assertEquals("1.94", NthSeries.seriesSum(15));
-//    }
+    @Test
+    public void test1() {
+        assertEquals("1.57", NthSeries.seriesSum(5));
+    }
+    @Test
+    public void test2() {
+        assertEquals("1.77", NthSeries.seriesSum(9));
+    }
+    @Test
+    public void test3() {
+        assertEquals("1.94", NthSeries.seriesSum(15));
+    }
 }
