057b354fa9faf3b0387134e40d989b71179e4b8a
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-10-05 14:49:53 +0200

    GREEN: Silly implementation
    Returned the hardcoded output base on "n".

--- src/main/java/com/alisarrian/_8kyu/NthSeries.java
+++ src/main/java/com/alisarrian/_8kyu/NthSeries.java
@@ -2,6 +2,9 @@ package com.alisarrian._8kyu;
 
 public class NthSeries {
     public static String seriesSum(int n) {
+        if (n == 3) {
+            return "1.39";
+        }
         if (n == 2) {
             return "1.25";
         }
